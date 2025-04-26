45ecffb469efadbefd61b8ca183ce94b2750f42a
workspace1111 <workspace.imagine@gmail.com>
2025-04-26 18:21:01 +0300

    Update trends_api.py

--- trends_api.py
+++ trends_api.py
@@ -5,12 +5,16 @@ app = Flask(__name__)
 
 @app.route('/trending')
 def trending():
-    pytrends = TrendReq(hl='tr-TR', tz=180)
-    trending_searches = pytrends.trending_searches(pn='turkey')  # 'turkey', 'united_states', 'worldwide' vs.
-    results = []
-    for i, row in trending_searches.iterrows():
-        results.append({"name": row[0]})
-    return jsonify(results)
+    try:
+        pytrends = TrendReq(hl='tr-TR', tz=180)
+        trending_searches = pytrends.trending_searches(pn='turkey')
+        results = []
+        for i, row in trending_searches.iterrows():
+            results.append({"name": row[0]})
+        return jsonify(results)
+    except Exception as e:
+        # Hata mesajını da döndür
+        return jsonify({"error": str(e)}), 500
 
 import os
 
